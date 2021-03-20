4231530b21dbb26b0db34430caddfce099dcb157
studioj <neefsj@gmail.com>
2021-03-20 11:18:00 +0100

    Add first functionality (#2)
    getting story points from an epic key

---
+++ jira_agile_toolbox/__init__.py
@@ -0,0 +1,47 @@
+__version_info__ = (0, 0, 3)
+__version__ = ".".join(map(str, __version_info__))
+
+
+class JiraAgileToolBox(object):
+    def __init__(self, jira_client):
+        """
+
+        :param jira_client: an instance of jira.JIRA
+        :type jira_client: jira.JIRA
+        """
+        self._jira_client = jira_client
+        self._story_points_custom_field = None
+        self._story_points_custom_field_name = "Story Points"
+
+    def get_storypoints_from_epic(self, epic):
+        """
+        searches for the epic and returns the number of storypoints as a dict
+
+        :param epic: the epic key or a jira Issue
+        :type epic: str jira.Issue
+        :return: a dictionary containing total story points
+        :rtype: dict
+        """
+        if not self._story_points_custom_field:
+            fields = self._jira_client.fields()
+            for field in fields:
+                if field["name"] == self._story_points_custom_field_name:
+                    self._story_points_custom_field = field["id"]
+
+        issues_in_epic = self._jira_client.search_issues(
+            "'Epic Link' = " + "PROJ001-001", fields=self._story_points_custom_field, maxResults=0
+        )
+        sum_of_story_points = 0
+        for issue in issues_in_epic:
+            if issue and getattr(issue.fields, self._story_points_custom_field, None):
+                sum_of_story_points += int(getattr(issue.fields, self._story_points_custom_field, 0))
+
+        all_states = list({issue.fields.status.name for issue in issues_in_epic if issue})
+
+        sum_of_story_points_per_state = {state: 0 for state in all_states}
+        for issue in issues_in_epic:
+            if issue and getattr(issue.fields, self._story_points_custom_field, None):
+                sum_of_story_points_per_state[issue.fields.status.name] += int(getattr(issue.fields, self._story_points_custom_field, 0))
+
+        sum_of_story_points_per_state["total"] = sum_of_story_points
+        return sum_of_story_points_per_state
---
+++ tests/test_epic_interactions.py
@@ -0,0 +1,328 @@
+import unittest
+from collections import namedtuple
+
+from unittest.mock import Mock, MagicMock
+
+from jira_agile_toolbox import JiraAgileToolBox
+
+
+class MockedJiraIssue(object):
+    def __init__(self, story_points=None, status="Reported"):
+        self.fields = MagicMock()
+        # self.fields.status = namedtuple("status", ["name"])
+        self.fields.customfield_10282 = story_points
+        self.fields.status.name = status
+
+
+class TestEpicStoryPointRetrieval(unittest.TestCase):
+    def test_get_story_points_from_epic_returns_a_dict_containing_the_total_story_points(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(story_points=0),
+            MockedJiraIssue(story_points=1),
+            MockedJiraIssue(story_points=2),
+        ]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        result = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertIn("total", result.keys())
+
+    def test_get_story_points_from_epic_looks_in_jira_for_all_children_of_the_epic(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(story_points=0),
+            MockedJiraIssue(story_points=1),
+            MockedJiraIssue(story_points=2),
+        ]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        jira_client.search_issues.assert_called_with("'Epic Link' = " + "PROJ001-001", fields="customfield_10282", maxResults=0)
+
+    def test_get_story_points_from_epic_calculates_the_total_story_pointS_for_3_issues(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(story_points=0),
+            MockedJiraIssue(story_points=1),
+            MockedJiraIssue(story_points=2),
+        ]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        sps = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertEqual(3, sps["total"])
+
+    def test_get_story_points_from_epic_calculates_the_total_story_pointS_for_300_issues(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(story_points=0),
+            MockedJiraIssue(story_points=1),
+            MockedJiraIssue(story_points=2),
+        ] * 100
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        sps = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertEqual(300, sps["total"])
+
+    def test_get_story_points_from_epic_calculates_the_total_of_zerostory_point_when_none_are_assigned(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(story_points=None),
+            MockedJiraIssue(),
+            MockedJiraIssue(),
+        ] * 1000
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        sps = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertEqual(0, sps["total"])
+
+    def test_get_story_points_from_epic_returns_a_dict_containing_the_found_statuses_as_keyword(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(0, "Reported"),
+            MockedJiraIssue(1, "Investigated"),
+            MockedJiraIssue(2, "Closed"),
+        ]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        result = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertIn("Reported", result.keys())
+        self.assertIn("Investigated", result.keys())
+        self.assertIn("Closed", result.keys())
+        self.assertNotIn("In Progress", result.keys())
+
+    def test_get_story_points_from_epic_returns_a_dict_containing_the_found_statuses_with_their_totals(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10282",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10282]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10282},
+            },
+        ]
+        jira_client.search_issues.return_value = [
+            MockedJiraIssue(0, "Reported"),
+            MockedJiraIssue(1, "Investigated"),
+            MockedJiraIssue(1, "Closed"),
+            MockedJiraIssue(1, "Closed"),
+        ]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        result = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        self.assertEqual({"total": 3, "Reported": 0, "Investigated": 1, "Closed": 2}, result)
+
+    def test_if_storypoints_custom_field_is_unknown_we_get_itfirst_via_the_fields_getter(self):
+        # Given
+        jira_client = Mock()
+        jira_client.fields.return_value = [
+            {
+                "id": "customfield_11280",
+                "name": "Release Note",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[11280]", "Release Note"],
+                "schema": {"type": "option", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:select", "customId": 11280},
+            },
+            {
+                "id": "customfield_10333",
+                "name": "Story Points",
+                "custom": True,
+                "orderable": True,
+                "navigable": True,
+                "searchable": True,
+                "clauseNames": ["cf[10333]", "Story Points"],
+                "schema": {"type": "number", "custom": "com.atlassian.jira.plugin.system.customfieldtypes:float", "customId": 10333},
+            },
+        ]
+        mocked_jira_issue = MockedJiraIssue()
+        mocked_jira_issue.fields.customfield_10333 = 100
+        mocked_jira_issue2 = MockedJiraIssue()
+        mocked_jira_issue2.fields.customfield_10333 = 0
+        jira_client.search_issues.return_value = [mocked_jira_issue2, mocked_jira_issue]
+        jat = JiraAgileToolBox(jira_client)
+
+        # When
+        result = jat.get_storypoints_from_epic("PROJ001-001")
+
+        # Then
+        jira_client.fields.assert_called_once()
+
+        jira_client.search_issues.assert_called_with("'Epic Link' = " + "PROJ001-001", fields="customfield_10333", maxResults=0)
+
+        self.assertEqual({"total": 100, "Reported": 100}, result)
