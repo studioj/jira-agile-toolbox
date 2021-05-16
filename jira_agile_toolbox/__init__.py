352e5f880b89d60d62c39552b5196490257d0f9e
studioj <22102283+studioj@users.noreply.github.com>
2021-05-16 02:07:55 +0200

    read the docs

--- jira_agile_toolbox/__init__.py
+++ jira_agile_toolbox/__init__.py
@@ -5,16 +5,28 @@ try:
     __version__ = get_distribution("package-name").version
 except DistributionNotFound:
     # package is not installed
-    pass
+    __version__ = "unknown"
 
 
 class JiraAgileToolBox(object):
-    def __init__(self, jira_client):
-        """
+    """
+    a class which helps you do agile things with jira
 
-        :param jira_client: an instance of jira.JIRA
-        :type jira_client: jira.JIRA
-        """
+    :param jira_client: an instance of jira.JIRA
+    :type jira_client: jira.JIRA
+
+
+    ``Example``
+
+        .. code-block:: python
+
+            >>> from jira import JIRA
+            >>> jira_client = JIRA("https://jira.atlassian.org")
+            >>> jat = JiraAgileToolBox(jira_client)
+
+    """
+
+    def __init__(self, jira_client):
         self._jira_client = jira_client
         self._story_points_custom_field = None
         self._story_points_custom_field_name = "Story Points"
@@ -27,9 +39,21 @@ class JiraAgileToolBox(object):
         :type epic: str jira.Issue
         :return: a dictionary containing total story points
         :rtype: dict
+
+        ``Example``
+
+            .. code-block:: python
+
+                >>> from jira_agile_toolbox import JiraAgileToolBox
+                >>> from jira import JIRA
+                >>> my_jira_client = JIRA("https://my-jira-server.com", basic_auth("MYUSERNAME","MYPASSWORD")
+                >>> tb = JiraAgileToolBox(my_jira_client)
+                >>> tb.get_storypoints_from_epic("JAT-001")
+                {'total': 100, "Reported": 50, "Closed": 50}
+
         """
         if not self._story_points_custom_field:
-            self._story_points_custom_field = self.get_custom_field_from_name(self._story_points_custom_field_name)
+            self._story_points_custom_field = self._get_custom_field_from_name(self._story_points_custom_field_name)
 
         epic_key = epic.key if isinstance(epic, jira.Issue) else epic
 
@@ -52,12 +76,11 @@ class JiraAgileToolBox(object):
         sum_of_story_points_per_state["total"] = sum_of_story_points
         return sum_of_story_points_per_state
 
-    def get_custom_field_from_name(self, name):
+    def _get_custom_field_from_name(self, name):
         """
         helper method to find custom fields
 
         :param name: name of the field you want the "customxxxxx" value from
-        :return:
         """
         for field in self._jira_client.fields():
             if field["name"] == name:
@@ -69,6 +92,30 @@ class JiraAgileToolBox(object):
 
         :param ranked_list: list of issues to be sorted by rank index 0 has highest rank
         :param on_top_of_issue: issue on top of which these issues need to land
+
+
+        ``Example``
+
+            .. code-block:: python
+
+                >>> from jira_agile_toolbox import JiraAgileToolBox
+                >>> from jira import JIRA
+                >>> my_jira_client = JIRA("https://my-jira-server.com", basic_auth("MYUSERNAME","MYPASSWORD")
+                >>> tb = JiraAgileToolBox(my_jira_client)
+                >>> tb.rank_issues_by_list([my_jira_client.issue("JAT-001"), my_jira_client.issue("JAT-003")], my_jira_client.issue("JAT-005"))
+
+        will rank issues like:
+
+        =======     =======
+         before      after
+        =======     =======
+        JAT-010     JAT-010
+        JAT-005     JAT-001
+        JAT-003     JAT-003
+        JAT-002     JAT-005
+        JAT-001     JAT-002
+        =======     =======
+
         """
         reversed_list = ranked_list[::-1]
         reversed_list.insert(0, on_top_of_issue)
@@ -83,6 +130,29 @@ class JiraAgileToolBox(object):
         :param ranked_list: a list of jira Issues
         :param project: project key
         :type project: str
+
+        ``Example``
+
+            .. code-block:: python
+
+                >>> from jira_agile_toolbox import JiraAgileToolBox
+                >>> from jira import JIRA
+                >>> my_jira_client = JIRA("https://my-jira-server.com", basic_auth("MYUSERNAME","MYPASSWORD")
+                >>> tb = JiraAgileToolBox(my_jira_client)
+                >>> tb.rank_issues_by_list([my_jira_client.issue("JAT-001"), my_jira_client.issue("JAT-003")])
+
+        will produce following result
+
+        =======     =======
+         before      after
+        =======     =======
+        JAT-010     JAT-001
+        JAT-005     JAT-003
+        JAT-003     JAT-010
+        JAT-002     JAT-005
+        JAT-001     JAT-002
+        =======     =======
+
         """
         issues_sorted_on_rank = self._jira_client.search_issues(f"project = { project } ORDER BY Rank ASC", fields="key", maxResults=1000)
         for issue in issues_sorted_on_rank:
