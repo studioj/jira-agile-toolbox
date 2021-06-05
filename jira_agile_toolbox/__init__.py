5b3c5d56bc7c2647d2bb69f1d2c613670ae40fb2
studioj <22102283+studioj@users.noreply.github.com>
2021-06-06 00:53:50 +0200

    Keeping original labels and versions or not (#16)
    * overwrite labels
    
    * overwrite versions

--- jira_agile_toolbox/__init__.py
+++ jira_agile_toolbox/__init__.py
@@ -203,7 +203,7 @@ class JiraAgileToolBox(object):
                 self.rank_issues_by_list(ranked_list, issue)
                 break
 
-    def add_labels_to_all_sub_items_of_epic(self, epic, labels):
+    def add_labels_to_all_sub_items_of_epic(self, epic, labels, keep_already_present=True):
         """
         adds labels to all 'Issues in Epic'
 
@@ -211,6 +211,8 @@ class JiraAgileToolBox(object):
         :type epic: str jira.Issue
         :param labels: the label to set as a string or the labels to set as a list
         :type labels: str list
+        :param keep_already_present: if this is set to False already present labels will be overwritten (defaults to True)
+        :type keep_already_present: bool
 
         ``Example``
 
@@ -227,8 +229,11 @@ class JiraAgileToolBox(object):
         labels_to_set = self._input_validation_labels(labels)
         items_to_update = self.get_all_issues_in_epic(epic, fields=["labels"])
         for item in items_to_update:
-            for label in labels_to_set:
-                item.add_field_value("labels", label)
+            if keep_already_present:
+                for label in labels_to_set:
+                    item.add_field_value("labels", label)
+            else:
+                item.update(fields={"labels": labels_to_set})
 
     def _input_validation_labels(self, labels):
         labels_to_set = []
@@ -248,13 +253,14 @@ class JiraAgileToolBox(object):
             raise ValueError(bad_input)
         return labels_to_set
 
-    def copy_fix_version_from_epic_to_all_items_in_epic(self, epic):
+    def copy_fix_version_from_epic_to_all_items_in_epic(self, epic, keep_already_present=True):
         """
         copies fixVersions from the epic to all 'Issues in Epic'
 
         :param epic: and epic key as a string or the epic as a jira.Issue
         :type epic: str jira.Issue
-        :return: None
+        :param keep_already_present: if this is set to False already present fixVersions will be overwritten (defaults to True)
+        :type keep_already_present: bool
 
         ``Example``
 
@@ -271,8 +277,12 @@ class JiraAgileToolBox(object):
                 >>> tb.get_all_issues_in_epic("JAT-001")[0].fields.fixVersions
                 [<JIRA Version: name='0.0.10', id='31063'>]
         """
-        jira_epic = epic  # if isinstance(epic, jira.Issue) else self._jira_client.issue(epic)
+        jira_epic = epic if isinstance(epic, jira.Issue) else self._jira_client.issue(epic)
         versions = [version.raw for version in jira_epic.fields.fixVersions]
         for issue in self.get_all_issues_in_epic(jira_epic, fields=["fixVersions"]):
-            for version in versions:
-                issue.add_field_value("fixVersions", version)
+
+            if keep_already_present:
+                for version in versions:
+                    issue.add_field_value("fixVersions", version)
+            else:
+                issue.update(fields={"fixVersions": versions})
